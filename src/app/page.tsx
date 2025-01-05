cdd19dc59a9baddc70e54cc12c069eb58d0eac8d
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-05 19:56:33 +0600

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main className="flex-center min-h-screen bg-sky-200">
+      <h1 className="text-green-400 text-4xl font-bold">Home Page</h1>
+    </main>
+  )
+}
+
+export default HomePage
