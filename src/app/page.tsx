4d4913f3ea9c8aa396ea979ada780d0966574feb
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-05 20:05:40 +0600

    Enhance Home Page layout with improved styling and container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
     <main className="flex-center min-h-screen bg-sky-200">
-      <h1 className="text-green-400 text-4xl font-bold">Home Page</h1>
+      <div className="w-full max-w-screen-2xl mx-auto p-20 bg-slate-200 rounded-xl shadow-lg">
+        <h1 className="text-green-600 text-4xl font-bold">Home Page</h1>
+      </div>
     </main>
   )
 }
